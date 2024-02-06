633fd27f941938f327544ea4cb9e39515c0700ed
Ana-MM <ana.marques@sou.inteli.edu.br>
2024-02-06 15:14:37 -0300

    criando modulo de funcoes

---
+++ funcoes.py
@@ -0,0 +1,2 @@
+def soma_dois_numeros(num1,num2):
+    return num1 + num2
--- ola.py
+++ ola.py
@@ -1,1 +1,5 @@
+from funcoes import soma_dois_numeros
 print ("olá mundo!")
+
+resultado = soma_dois_numeros(5,9.3)
+print(f"O resultado da soma é: {resultado}")
