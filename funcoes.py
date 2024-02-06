cb862de1308051b2b7975365d11343af404c46a0
Ana-MM <ana.marques@sou.inteli.edu.br>
2024-02-06 15:31:06 -0300

    criando funcao de multiplicar

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,5 @@
 def soma_dois_numeros(num1,num2):
     return num1 + num2
+
+def multiplica_dois_numeros(num1,num2):
+    return num1 * num2
--- ola.py
+++ ola.py
@@ -1,5 +1,6 @@
-from funcoes import soma_dois_numeros
+from funcoes import soma_dois_numeros, multiplica_dois_numeros
 print ("olá mundo!")
 
-resultado = soma_dois_numeros(5,9.3)
-print(f"O resultado da soma é: {resultado}")
+resultado_da_soma = soma_dois_numeros(5,9.3)
+resultado_da_multiplicacao = multiplica_dois_numeros(5,9.3)
+print(f"O resultado da soma é: {resultado_da_soma} e da mutiplicação é {resultado_da_multiplicacao}")
